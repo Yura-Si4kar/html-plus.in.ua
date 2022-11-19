742c5589a911a497eaa5a3b5c87d8f82ae6e4d44
Sichkar Yurii <si4kar2017@gmail.com>
2022-11-19 22:07:27 +0200

    task_4

---
+++ Task_4/script.js
@@ -0,0 +1,18 @@
+let userNameP = '<div class="input_text">';
+let userNameList = '<ol class="input_list">';
+
+getName();
+
+function getName() {
+  for(let i=0; i<4; i++) {
+    userName = prompt('Enter your name!');
+    userNameP += `<p>${userName}</p>`;
+    userNameList += `<li>${userName}</li>`;
+  }
+  
+  userNameP += '</div>'
+  userNameList += '</ol>';
+
+  document.write(userNameP);
+  document.write(userNameList);
+}
