34536a3ff69ad530f1ba5d62db8e2742fc38916d
Sichkar Yurii <si4kar2017@gmail.com>
2022-11-19 22:31:27 +0200

    task_6

---
+++ Task_6/script.js
@@ -0,0 +1,9 @@
+let name = prompt('Enter Your name!');
+let birthYear = prompt('Enter the year of birth!');
+
+showMessage(name, birthYear);
+// Виправити!!! Не вірно рахує, якщо день народження ще не наступив
+function showMessage(name, date) {
+  let currentDate = new Date().getFullYear() - date;
+  return document.write(`${name}: ${currentDate}`)
+};
