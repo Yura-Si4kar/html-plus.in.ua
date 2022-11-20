35c659012d5392db1e9db2c5876d1a514d5e497c
Sichkar Yurii <96392712+Yura-Si4kar@users.noreply.github.com>
2022-11-20 18:39:06 +0200

    task_7

--- Task_7/script.js
+++ Task_7/script.js
@@ -5,11 +5,8 @@ alert('До результату додайте 7!');
 calculate();
 
 function calculate() {
-    let number;
-    let result;
-  
-    number = +prompt('Введіть результат!');
-    result = (number-7)/2 <= 0 ? alert('Число має бути більше 0') : (number-7)/2;
+    let number = +prompt('Введіть результат!');
+    let result = (number-7)/2 <= 0 ? alert('Число має бути більше 0') : (number-7)/2;
   
     return alert(`Число, яке ви загадали ${result}`);
 }
