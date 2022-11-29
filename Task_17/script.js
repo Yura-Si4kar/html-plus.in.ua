4914b96d820c3e5519919ed19ea1e0f06651b68a
Sichkar Yurii <si4kar2017@gmail.com>
2022-11-29 16:14:11 +0200

    task_24

--- Task_17/script.js
+++ Task_17/script.js
@@ -1,6 +1,6 @@
 for (let i = 1; i < 7; i++) {
-    let first = Math.floor(0 + Math.random() * (255 + 1 - 0));
-    let second = Math.floor(0 + Math.random() * (255 + 1 - 0));
-    let third = Math.floor(0 + Math.random() * (255 + 1 - 0));
+    let first = Math.floor(Math.random() * 255);
+    let second = Math.floor(Math.random() * 255);
+    let third = Math.floor(Math.random() * 255);
     document.write(`<p class="text" style="background-color: rgb(${first}, ${second}, ${third})">Абзац ${i}</p>`)
 }
